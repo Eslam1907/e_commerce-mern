eb15a245c67ee0d5789d101f09037a6d1ac3f8de
eslam <eslammoh907@gmail.com>
2024-05-04 03:34:42 +0200

    changes image link

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ app.use("/images", express.static("upload/images"));
 app.post("/upload/", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+    image_url: `https://e-commerce-mern-2-xjic.onrender.com//images/${req.file.filename}`,
   });
 });
 // schema for creating products
